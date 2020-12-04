ce0d75b952e286ffd2f449501d63853fbf999690
Austin Scola <austinscola@gmail.com>
2020-12-04 18:47:40 -0500

    Add argument parsing setting up

--- illud/main.py
+++ illud/main.py
@@ -1,7 +1,16 @@
 """Main entry point."""
+import argparse
 from typing import List
+
+
+def _set_up_argument_parser() -> argparse.ArgumentParser:
+    """Set up an argument parser."""
+    argument_parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        prog='python3 -m illud', description='A text buffer editor and terminal viewer.')
+    return argument_parser
 
 
 def main(arguments: List[str]) -> int:  # pylint: disable=unused-argument
     """Main entry point."""
+    argument_parser: argparse.ArgumentParser = _set_up_argument_parser()  # pylint: disable=unused-variable
     return 0
