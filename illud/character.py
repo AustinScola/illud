0acbfc497569f9d420e4b1eb2c8e60ab1b14ad7b
Austin Scola <austinscola@gmail.com>
2020-12-06 08:38:35 -0500

    Add character
    Add character which is a string with length of 1.

---
+++ illud/character.py
@@ -0,0 +1,13 @@
+"""A single string character."""
+from typing import Any
+
+
+class Character():  # pylint: disable=too-few-public-methods
+    """A single string character."""
+    def __init__(self, value: str):
+        self.value: str = value
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Character):
+            return False
+        return self.value == other.value
