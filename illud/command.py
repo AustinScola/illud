5f176c708cde149c2014081c78a2792d01b8a874
Austin Scola <austinscola@gmail.com>
2021-01-01 08:09:30 -0500

    Add command

---
+++ illud/command.py
@@ -0,0 +1,17 @@
+"""A key command."""
+from typing import Any
+from illud.character import Character
+
+
+class Command():
+    """A key command."""
+    def __init__(self, character: Character):
+        self.character: Character = character
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Command):
+            return False
+        return self.character == other.character
+
+    def __str__(self) -> str:
+        return self.character.value
