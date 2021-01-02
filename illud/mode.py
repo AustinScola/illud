76d6d5db25ffcbd10d5c73fc01cb590c9cd6f072
Austin Scola <austinscola@gmail.com>
2021-01-02 08:56:55 -0500

    Add mode
    Add mode to represent a manner of operation.

---
+++ illud/mode.py
@@ -0,0 +1,5 @@
+"""A manner of operation."""
+
+
+class Mode():  # pylint: disable=too-few-public-methods
+    """A manner of operation."""
---
+++ tests/test_mode.py
@@ -0,0 +1,2 @@
+"""Test illud.mode."""
+from illud.mode import Mode  # pylint: disable=unused-import
