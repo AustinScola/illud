2073865f649da8d6ccbe25d2f1116e0b9cf2ec12
Austin Scola <austinscola@gmail.com>
2021-01-02 08:33:30 -0500

    Add buffer equality operator
    Add the equality operator for string buffers.

--- illud/buffer.py
+++ illud/buffer.py
@@ -1,7 +1,14 @@
 """A string buffer."""
+from typing import Any
 
 
 class Buffer():  # pylint: disable=too-few-public-methods
     """A string buffer."""
     def __init__(self, string: str):
         self.string = string
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Buffer):
+            return False
+
+        return self.string == other.string
--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -1,4 +1,6 @@
 """Test illud.buffer."""
+from typing import Any
+
 import pytest
 
 from illud.buffer import Buffer
@@ -15,3 +17,18 @@ def test_init(string: str) -> None:
     buffer_: Buffer = Buffer(string)
 
     assert buffer_.string == string
+
+
+# yapf: disable
+@pytest.mark.parametrize('buffer_, other, expected_equality', [
+    (Buffer(''), 'foo', False),
+    (Buffer(''), Buffer('foo'), False),
+    (Buffer(''), Buffer(''), True),
+    (Buffer('foo'), Buffer('foo'), True),
+])
+# yapf: enable
+def test_eq(buffer_: Buffer, other: Any, expected_equality: bool) -> None:
+    """Test illud.buffer.Buffer.__eq__."""
+    equality: bool = buffer_ == other
+
+    assert equality == expected_equality
