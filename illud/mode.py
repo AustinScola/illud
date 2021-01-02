2797e0564fa7a338c0d90d807ce7f043e203d588
Austin Scola <austinscola@gmail.com>
2021-01-02 13:56:38 -0500

    Add mode equality operator
    Add the equality operator for modes.

--- illud/mode.py
+++ illud/mode.py
@@ -1,5 +1,8 @@
 """A manner of operation."""
+from typing import Any
 
 
 class Mode():  # pylint: disable=too-few-public-methods
     """A manner of operation."""
+    def __eq__(self, other: Any) -> bool:
+        return type(self) is type(other)
