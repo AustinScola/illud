a7705b609a1a67a7e91f401985a4667a16037d2e
Austin Scola <austinscola@gmail.com>
2021-01-03 15:10:13 -0500

    Add evaluation of "i" command in normal mode
    Add evaluation of "i" command in normal mode to switch to insert mode.

--- illud/modes/normal.py
+++ illud/modes/normal.py
@@ -1,6 +1,18 @@
 """Mode for navigating and manipulating text."""
+from typing import TYPE_CHECKING
+
+from illud.character import Character
+from illud.command import Command
 from illud.mode import Mode
+from illud.modes.insert import Insert
+
+if TYPE_CHECKING:
+    from illud.illud_state import IlludState  # pylint: disable=cyclic-import
 
 
 class Normal(Mode):  # pylint: disable=too-few-public-methods
     """Mode for navigating and manipulating text."""
+    @staticmethod
+    def evaluate(state: 'IlludState', command: Command) -> None:
+        if command == Command(Character('i')):
+            state.mode = Insert()
