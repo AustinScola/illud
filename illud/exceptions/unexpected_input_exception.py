85fe756134ce7a403eb9feae0e432151dcb15f50
Austin Scola <austinscola@gmail.com>
2021-01-15 16:57:22 -0500

    Add unexpected input exception
    Add an unexpected input exception to be raised when there is an
    unexpected input.

---
+++ illud/exceptions/unexpected_input_exception.py
@@ -0,0 +1,6 @@
+"""Raised when there is an unexpected input."""
+from illud.exception import IlludException
+
+
+class UnexpectedInputException(IlludException):
+    """Raised when there is an unexpected input."""
