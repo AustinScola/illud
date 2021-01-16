ceda741f47062a4adb0aaea072c9ae6480cb5fdb
Austin Scola <austinscola@gmail.com>
2021-01-16 15:50:41 -0500

    Add get move cursor ANSI escape code
    Add the ability to get an ANSI escape code for moving the cursor
    position.

--- illud/ansi/escape_codes/cursor.py
+++ illud/ansi/escape_codes/cursor.py
@@ -1,4 +1,20 @@
 """ANSI escape codes for the cursor position."""
 from illud.ansi.escape_codes.control import CONTROL_SEQUENCE_INTRODUCER
+from illud.integer_position_2d import IntegerPosition2D
 
 DEVICE_STATUS_REPORT = CONTROL_SEQUENCE_INTRODUCER + '6n'
+
+
+def get_move_cursor(position: IntegerPosition2D) -> str:
+    """Return an escape code to move the cursor to the given position."""
+    one_based_position = position + IntegerPosition2D(1, 1)
+
+    move_cursor_command: str = CONTROL_SEQUENCE_INTRODUCER
+    if one_based_position.y != 1:
+        move_cursor_command += str(one_based_position.y)
+    move_cursor_command += ';'
+    if one_based_position.x != 1:
+        move_cursor_command += str(one_based_position.x)
+    move_cursor_command += 'H'
+
+    return move_cursor_command
--- tests/ansi/escape_codes/test_cursor.py
+++ tests/ansi/escape_codes/test_cursor.py
@@ -1,7 +1,27 @@
 """Test illud.ansi.escape_codes.cursor."""
-from illud.ansi.escape_codes.cursor import DEVICE_STATUS_REPORT
+import pytest
+
+from illud.ansi.escape_codes.cursor import DEVICE_STATUS_REPORT, get_move_cursor
+from illud.integer_position_2d import IntegerPosition2D
 
 
 def test_clear_screen() -> None:
     """Test illud.ansi.escape_codes.cursor.DEVICE_STATUS_REPORT."""
     assert DEVICE_STATUS_REPORT == '\x1b[6n'
+
+
+# yapf: disable
+@pytest.mark.parametrize('position, expected_move_cursor_command', [
+    (IntegerPosition2D(0,0), '\x1b[;H'),
+    (IntegerPosition2D(0,1), '\x1b[2;H'),
+    (IntegerPosition2D(1,0), '\x1b[;2H'),
+    (IntegerPosition2D(1,1), '\x1b[2;2H'),
+    (IntegerPosition2D(1,7), '\x1b[8;2H'),
+    (IntegerPosition2D(7,1), '\x1b[2;8H'),
+    (IntegerPosition2D(7,7), '\x1b[8;8H'),
+])
+# yapf: enable
+def test_get_move_cursor(position: IntegerPosition2D, expected_move_cursor_command: str) -> None:
+    """Test illud.ansi.escape_codes.cursor.get_move_cursor."""
+    move_cusor_command: str = get_move_cursor(position)
+    assert move_cusor_command == expected_move_cursor_command
