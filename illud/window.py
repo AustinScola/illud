c8515d0805fca7907273f781c27ea5c455f16be6
Austin Scola <austinscola@gmail.com>
2021-01-21 06:18:02 -0500

    Add window rows and columns
    Add properties to windows for iterating the rows and columns.

--- illud/window.py
+++ illud/window.py
@@ -1,12 +1,24 @@
 """A rectangular view of a string buffer."""
+from typing import Iterable
+
 from illud.buffer import Buffer
 from illud.integer_position_2d import IntegerPosition2D
 
 
-class Window():  # pylint: disable=too-few-public-methods
+class Window():
     """A rectangular view of a string buffer."""
     def __init__(self, position: IntegerPosition2D, width: int, height: int, buffer_: Buffer):
         self.position: IntegerPosition2D = position
         self.width: int = width
         self.height: int = height
         self.buffer: Buffer = buffer_
+
+    @property
+    def rows(self) -> Iterable[int]:
+        """Return an iterator of the rows of the window."""
+        return range(self.position.y, self.position.y + self.height)
+
+    @property
+    def columns(self) -> Iterable[int]:
+        """Return an iterator of the columnss of the window."""
+        return range(self.position.x, self.position.x + self.width)
--- tests/test_window.py
+++ tests/test_window.py
@@ -1,4 +1,6 @@
 """Test illud.window."""
+from typing import Iterable
+
 import pytest
 
 from illud.buffer import Buffer
@@ -19,3 +21,37 @@ def test_init(position: IntegerPosition2D, width: int, height: int, buffer_: Buf
     assert window.width == width
     assert window.height == height
     assert window.buffer is buffer_
+
+
+# yapf: disable
+@pytest.mark.parametrize('window, expected_rows', [
+    (Window(IntegerPosition2D(0, 0), 0, 0, Buffer('')), range(0, 0)),
+    (Window(IntegerPosition2D(0, 0), 0, 3, Buffer('')), range(0, 3)),
+    (Window(IntegerPosition2D(0, 1), 0, 0, Buffer('')), range(1, 1)),
+    (Window(IntegerPosition2D(0, 1), 0, 3, Buffer('')), range(1, 4)),
+    (Window(IntegerPosition2D(0, 7), 0, 0, Buffer('')), range(7, 7)),
+    (Window(IntegerPosition2D(0, 7), 0, 3, Buffer('')), range(7, 10)),
+])
+# yapf: enable
+def test_rows(window: Window, expected_rows: Iterable[int]) -> None:
+    """Test illud.window.Window.rows"""
+    rows: Iterable[int] = window.rows
+
+    assert rows == expected_rows
+
+
+# yapf: disable
+@pytest.mark.parametrize('window, expected_columns', [
+    (Window(IntegerPosition2D(0, 0), 0, 0, Buffer('')), range(0, 0)),
+    (Window(IntegerPosition2D(0, 0), 3, 0, Buffer('')), range(0, 3)),
+    (Window(IntegerPosition2D(1, 0), 0, 0, Buffer('')), range(1, 1)),
+    (Window(IntegerPosition2D(1, 0), 3, 0, Buffer('')), range(1, 4)),
+    (Window(IntegerPosition2D(7, 0), 0, 0, Buffer('')), range(7, 7)),
+    (Window(IntegerPosition2D(7, 0), 3, 0, Buffer('')), range(7, 10)),
+])
+# yapf: enable
+def test_columns(window: Window, expected_columns: Iterable[int]) -> None:
+    """Test illud.window.Window.columns"""
+    columns: Iterable[int] = window.columns
+
+    assert columns == expected_columns
