a28a4647e372ae493e661310c0d5fdd385dcb81d
Austin Scola <austinscola@gmail.com>
2021-01-21 07:02:41 -0500

    Add buffer index
    Add the ability to find a substring in a buffer optionally within some
    range.

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -1,5 +1,5 @@
 """Test illud.buffer."""
-from typing import Any, List, Optional, Type, Union
+from typing import Any, Dict, List, Optional, Type, Union
 
 import pytest
 
@@ -64,3 +64,47 @@ def test_getitem(buffer_: Buffer, index_or_slice: Union[int, slice],
         item: Union[str, List[str]] = buffer_.__getitem__(index_or_slice)
 
         assert item == expected_item
+
+
+# yapf: disable # pylint: disable=line-too-long
+@pytest.mark.parametrize('buffer_, substring, start, pass_start, end, pass_end, expected_index, expected_exception', [
+    (Buffer(), '', None, False, None, False, 0, None),
+    (Buffer(), '', None, False, None, True, 0, None),
+    (Buffer(), '', None, True, None, False, 0, None),
+    (Buffer(), '', None, True, None, True, 0, None),
+    (Buffer(), 'foo', None, False, None, False, None, ValueError),
+    (Buffer(), 'foo', None, False, None, True, None, ValueError),
+    (Buffer(), 'foo', None, True, None, False, None, ValueError),
+    (Buffer(), 'foo', None, True, None, True, None, ValueError),
+    (Buffer('foo'), 'foo', None, False, None, False, 0, None),
+    (Buffer('foo'), 'foo', None, False, None, True, 0, None),
+    (Buffer('foo'), 'foo', None, True, None, False, 0, None),
+    (Buffer('foo'), 'foo', None, True, None, True, 0, None),
+    (Buffer('foobar'), 'bar', None, False, None, False, 3, None),
+    (Buffer('foobar'), 'foo', 3, True, None, False, None, ValueError),
+    (Buffer('foobar'), 'foo', None, False, 1, True, None, ValueError),
+    (Buffer('foobar'), 'foo', 0, True, 1, True, None, ValueError),
+    (Buffer('foobar'), 'baz', None, False, None, False, None, ValueError),
+])
+# yapf: enable # pylint: enable=line-too-long
+# pylint: disable=too-many-arguments
+def test_index(buffer_: Buffer, substring: str, start: Optional[int], pass_start: bool,
+               end: Optional[int], pass_end: bool, expected_index: Optional[int],
+               expected_exception: Optional[Type[Exception]]) -> None:
+    """Test illud.buffer.Buffer.index."""
+    keyword_arguments: Dict[str, Any] = {}
+    if pass_start:
+        keyword_arguments['start'] = start
+    if pass_end:
+        keyword_arguments['end'] = end
+
+    if expected_exception is not None:
+        with pytest.raises(expected_exception):
+            buffer_.index(substring, **keyword_arguments)
+    else:
+        index = buffer_.index(substring, **keyword_arguments)
+
+        assert index == expected_index
+
+
+# pylint: enable=too-many-arguments
