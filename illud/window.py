8c94a834d7961c10744b3e186487570fbe15320e
Austin Scola <austinscola@gmail.com>
2021-01-22 07:12:59 -0500

    Add window edge columns and rows
    Add to windows the left column, right column, top row, and bottom row.

--- illud/window.py
+++ illud/window.py
@@ -2,6 +2,8 @@
 from typing import Iterable
 
 from illud.buffer import Buffer
+from illud.exceptions.no_columns_exception import NoColumnsException
+from illud.exceptions.no_rows_exception import NoRowsException
 from illud.integer_position_2d import IntegerPosition2D
 
 
@@ -22,3 +24,39 @@ class Window():
     def columns(self) -> Iterable[int]:
         """Return an iterator of the columnss of the window."""
         return range(self.position.x, self.position.x + self.width)
+
+    @property
+    def left_column(self) -> int:
+        """Return the integer position of the left most column or raise an error if there are no
+        columns."""
+        if not self.width:
+            raise NoColumnsException
+
+        return self.position.x
+
+    @property
+    def right_column(self) -> int:
+        """Return the integer position of the right most column or raise an error if there are no
+        columns."""
+        if not self.width:
+            raise NoColumnsException
+
+        return self.position.x + self.width - 1
+
+    @property
+    def top_row(self) -> int:
+        """Return the integer position of the top most row or raise an error if there are no
+        rows."""
+        if not self.height:
+            raise NoRowsException
+
+        return self.position.y
+
+    @property
+    def bottom_row(self) -> int:
+        """Return the integer position of the bottom most row or raise an error if there are no
+        rows."""
+        if not self.height:
+            raise NoRowsException
+
+        return self.position.y + self.height - 1
--- tests/test_window.py
+++ tests/test_window.py
@@ -1,9 +1,11 @@
 """Test illud.window."""
-from typing import Iterable
+from typing import Iterable, Optional, Type
 
 import pytest
 
 from illud.buffer import Buffer
+from illud.exceptions.no_columns_exception import NoColumnsException
+from illud.exceptions.no_rows_exception import NoRowsException
 from illud.integer_position_2d import IntegerPosition2D
 from illud.window import Window
 
@@ -55,3 +57,103 @@ def test_columns(window: Window, expected_columns: Iterable[int]) -> None:
     columns: Iterable[int] = window.columns
 
     assert columns == expected_columns
+
+
+# yapf: disable
+@pytest.mark.parametrize('window, expected_left_column, expected_exception', [
+    (Window(IntegerPosition2D(0, 0), 0, 0, Buffer()), None, NoColumnsException),
+    (Window(IntegerPosition2D(1, 0), 0, 0, Buffer()), None, NoColumnsException),
+    (Window(IntegerPosition2D(3, 0), 0, 0, Buffer()), None, NoColumnsException),
+    (Window(IntegerPosition2D(0, 0), 1, 0, Buffer()), 0, None),
+    (Window(IntegerPosition2D(1, 0), 1, 0, Buffer()), 1, None),
+    (Window(IntegerPosition2D(3, 0), 1, 0, Buffer()), 3, None),
+    (Window(IntegerPosition2D(0, 0), 3, 0, Buffer()), 0, None),
+    (Window(IntegerPosition2D(1, 0), 3, 0, Buffer()), 1, None),
+    (Window(IntegerPosition2D(3, 0), 3, 0, Buffer()), 3, None),
+])
+# yapf: enable
+def test_left_column(window: Window, expected_left_column: Optional[int],
+                     expected_exception: Optional[Type[Exception]]) -> None:
+    """Test illud.window.Window.left_column."""
+    if expected_exception:
+        with pytest.raises(expected_exception):
+            window.left_column  # pylint: disable=pointless-statement
+    else:
+        left_column: int = window.left_column
+
+        assert left_column == expected_left_column
+
+
+# yapf: disable
+@pytest.mark.parametrize('window, expected_right_column, expected_exception', [
+    (Window(IntegerPosition2D(0, 0), 0, 0, Buffer()), None, NoColumnsException),
+    (Window(IntegerPosition2D(1, 0), 0, 0, Buffer()), None, NoColumnsException),
+    (Window(IntegerPosition2D(3, 0), 0, 0, Buffer()), None, NoColumnsException),
+    (Window(IntegerPosition2D(0, 0), 1, 0, Buffer()), 0, None),
+    (Window(IntegerPosition2D(1, 0), 1, 0, Buffer()), 1, None),
+    (Window(IntegerPosition2D(3, 0), 1, 0, Buffer()), 3, None),
+    (Window(IntegerPosition2D(0, 0), 3, 0, Buffer()), 2, None),
+    (Window(IntegerPosition2D(1, 0), 3, 0, Buffer()), 3, None),
+    (Window(IntegerPosition2D(3, 0), 3, 0, Buffer()), 5, None),
+])
+# yapf: enable
+def test_right_column(window: Window, expected_right_column: Optional[int],
+                      expected_exception: Optional[Type[Exception]]) -> None:
+    """Test illud.window.Window.right_column."""
+    if expected_exception:
+        with pytest.raises(expected_exception):
+            window.right_column  # pylint: disable=pointless-statement
+    else:
+        right_column: int = window.right_column
+
+        assert right_column == expected_right_column
+
+
+# yapf: disable
+@pytest.mark.parametrize('window, expected_top_row, expected_exception', [
+    (Window(IntegerPosition2D(0, 0), 0, 0, Buffer()), None, NoRowsException),
+    (Window(IntegerPosition2D(0, 1), 0, 0, Buffer()), None, NoRowsException),
+    (Window(IntegerPosition2D(0, 3), 0, 0, Buffer()), None, NoRowsException),
+    (Window(IntegerPosition2D(0, 0), 0, 1, Buffer()), 0, None),
+    (Window(IntegerPosition2D(0, 1), 0, 1, Buffer()), 1, None),
+    (Window(IntegerPosition2D(0, 3), 0, 1, Buffer()), 3, None),
+    (Window(IntegerPosition2D(0, 0), 0, 3, Buffer()), 0, None),
+    (Window(IntegerPosition2D(0, 1), 0, 3, Buffer()), 1, None),
+    (Window(IntegerPosition2D(0, 3), 0, 3, Buffer()), 3, None),
+])
+# yapf: enable
+def test_top_row(window: Window, expected_top_row: Optional[int],
+                 expected_exception: Optional[Type[Exception]]) -> None:
+    """Test illud.window.Window.top_row."""
+    if expected_exception:
+        with pytest.raises(expected_exception):
+            window.top_row  # pylint: disable=pointless-statement
+    else:
+        top_row: int = window.top_row
+
+        assert top_row == expected_top_row
+
+
+# yapf: disable
+@pytest.mark.parametrize('window, expected_bottom_row, expected_exception', [
+    (Window(IntegerPosition2D(0, 0), 0, 0, Buffer()), None, NoRowsException),
+    (Window(IntegerPosition2D(0, 1), 0, 0, Buffer()), None, NoRowsException),
+    (Window(IntegerPosition2D(0, 3), 0, 0, Buffer()), None, NoRowsException),
+    (Window(IntegerPosition2D(0, 0), 0, 1, Buffer()), 0, None),
+    (Window(IntegerPosition2D(0, 1), 0, 1, Buffer()), 1, None),
+    (Window(IntegerPosition2D(0, 3), 0, 1, Buffer()), 3, None),
+    (Window(IntegerPosition2D(0, 0), 0, 3, Buffer()), 2, None),
+    (Window(IntegerPosition2D(0, 1), 0, 3, Buffer()), 3, None),
+    (Window(IntegerPosition2D(0, 3), 0, 3, Buffer()), 5, None),
+])
+# yapf: enable
+def test_bottom_row(window: Window, expected_bottom_row: Optional[int],
+                    expected_exception: Optional[Type[Exception]]) -> None:
+    """Test illud.window.Window.bottom_row."""
+    if expected_exception:
+        with pytest.raises(expected_exception):
+            window.bottom_row  # pylint: disable=pointless-statement
+    else:
+        bottom_row: int = window.bottom_row
+
+        assert bottom_row == expected_bottom_row
