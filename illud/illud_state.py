c87d592e30d3c80cc18db11bc75082b16aa29a42
Austin Scola <austinscola@gmail.com>
2021-01-25 06:13:51 -0500

    Add cursor to state
    Add the cursor to the Illud state.

--- illud/illud_state.py
+++ illud/illud_state.py
@@ -2,6 +2,7 @@
 from typing import Any, Optional
 
 from illud.buffer import Buffer
+from illud.cursor import Cursor
 from illud.mode import Mode
 from illud.modes.normal import Normal
 from illud.state import State
@@ -9,12 +10,21 @@ from illud.state import State
 
 class IlludState(State):  # pylint: disable=too-few-public-methods
     """Persistent information of Illud."""
-    def __init__(self, buffer_: Optional[Buffer] = None, mode: Mode = None):
+    def __init__(self,
+                 buffer_: Optional[Buffer] = None,
+                 cursor_position: Optional[int] = None,
+                 mode: Mode = None):
         self.buffer: Buffer
         if buffer_ is None:
             self.buffer = Buffer()
         else:
             self.buffer = buffer_
+
+        self.cursor: Cursor
+        if cursor_position is None:
+            self.cursor = Cursor(self.buffer, 0)
+        else:
+            self.cursor = Cursor(self.buffer, cursor_position)
 
         self.mode: Mode
         if mode is None:
--- tests/test_illud_state.py
+++ tests/test_illud_state.py
@@ -4,6 +4,7 @@ from typing import Any, Dict, Optional
 import pytest
 
 from illud.buffer import Buffer
+from illud.cursor import Cursor
 from illud.illud_state import IlludState
 from illud.mode import Mode
 from illud.modes.insert import Insert
@@ -16,31 +17,37 @@ def test_inheritance() -> None:
     assert issubclass(IlludState, State)
 
 
-# yapf: disable
-@pytest.mark.parametrize('buffer_, mode, pass_buffer, pass_mode, expected_buffer, expected_mode', [
-    (None, None, False, False, Buffer(), Normal()),
-    (None, Normal(), False, True, Buffer(), Normal()),
-    (Buffer(), None, True, False, Buffer(), Normal()),
-    (Buffer(), Normal(), True, True, Buffer(), Normal()),
-    (None, None, True, False, Buffer(), Normal()),
-    (None, Normal(), True, True, Buffer(), Normal()),
-    (Buffer('foo'), None, True, False, Buffer('foo'), Normal()),
-    (Buffer('foo'), Normal(), True, True, Buffer('foo'), Normal()),
+# yapf: disable # pylint: disable=line-too-long
+@pytest.mark.parametrize('buffer_, cursor_position, mode, pass_buffer, pass_cursor_position, pass_mode, expected_buffer, expected_cursor, expected_mode', [
+    (None, None, None, False, False,False, Buffer(), Cursor(Buffer(), 0), Normal()),
+    (None, None, Normal(), False, False, True, Buffer(), Cursor(Buffer(), 0), Normal()),
+    (Buffer(), None, None, True, False, False, Buffer(), Cursor(Buffer(), 0), Normal()),
+    (Buffer(), None, Normal(), True, False, True, Buffer(), Cursor(Buffer(), 0), Normal()),
+    (None, None, None, True, False, False, Buffer(), Cursor(Buffer(), 0), Normal()),
+    (None, None, Normal(), True, False, True, Buffer(), Cursor(Buffer(), 0), Normal()),
+    (Buffer('foo'), None, None, True, False, False, Buffer('foo'), Cursor(Buffer('foo'), 0), Normal()),
+    (Buffer('foo'), None, Normal(), True, False, True, Buffer('foo'), Cursor(Buffer('foo'), 0), Normal()),
+    (Buffer('foo'), 1, Normal(), True, True, True, Buffer('foo'), Cursor(Buffer('foo'), 1), Normal()),
 ])
-# yapf: enable
+# yapf: enable # pylint: enable=line-too-long
 # pylint: disable=too-many-arguments
-def test_init(buffer_: Optional[Buffer], mode: Optional[Mode], pass_buffer: bool, pass_mode: bool,
-              expected_buffer: Buffer, expected_mode: Mode) -> None:
+def test_init(buffer_: Optional[Buffer], cursor_position: Optional[int], mode: Optional[Mode],
+              pass_buffer: bool, pass_cursor_position: bool, pass_mode: bool,
+              expected_buffer: Buffer, expected_cursor: Cursor, expected_mode: Mode) -> None:
     """Test illud.illud_state.IlludState.__init__."""
     keyword_arguments: Dict[str, Any] = {}
     if pass_buffer:
         keyword_arguments['buffer_'] = buffer_
+    if pass_cursor_position:
+        keyword_arguments['cursor_position'] = cursor_position
     if pass_mode:
         keyword_arguments['mode'] = mode
 
     illud_state: IlludState = IlludState(**keyword_arguments)
 
     assert illud_state.buffer == expected_buffer
+    assert illud_state.cursor == expected_cursor
+    assert illud_state.cursor.buffer is illud_state.buffer
     assert illud_state.mode == expected_mode
 
 
@@ -53,8 +60,8 @@ def test_init(buffer_: Optional[Buffer], mode: Optional[Mode], pass_buffer: bool
     (IlludState(Buffer('foo')), IlludState(Buffer('foo')), True),
     (IlludState(mode=Normal()), IlludState(mode=Insert()), False),
     (IlludState(mode=Normal()), IlludState(mode=Normal()), True),
-    (IlludState(Buffer('foo'), mode=Normal()), IlludState(Buffer('bar'), Normal()), False),
-    (IlludState(Buffer('foo'), mode=Normal()), IlludState(Buffer('foo'), Normal()), True),
+    (IlludState(Buffer('foo'), mode=Normal()), IlludState(Buffer('bar'), mode=Normal()), False),
+    (IlludState(Buffer('foo'), mode=Normal()), IlludState(Buffer('foo'), mode=Normal()), True),
 ])
 # yapf: enable
 def test_eq(illud_state: IlludState, other: Any, expected_equality: bool) -> None:
