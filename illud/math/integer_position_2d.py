4bd54a9f28973fc8aee92a60900e50030a5b57bc
Austin Scola <austinscola@gmail.com>
2021-01-28 07:31:06 -0500

    Change integer position 2D inheritance
    Change integer postion 2D to be a vector 2 of integers.

--- illud/math/integer_position_2d.py
+++ illud/math/integer_position_2d.py
@@ -1,24 +1,6 @@
 """A position in the two-dimensional integer lattice."""
-from typing import Any
+from illud.math.vector_2 import Vector2
 
 
-class IntegerPosition2D():
+class IntegerPosition2D(Vector2[int]):  # pylint: disable=too-few-public-methods
     """A position in the two-dimensional integer lattice."""
-    def __init__(self, x: int, y: int):
-        self.x: int = x  # pylint: disable=invalid-name
-        self.y: int = y  # pylint: disable=invalid-name
-
-    def __eq__(self, other: Any) -> bool:
-        if not isinstance(other, IntegerPosition2D):
-            return False
-
-        return self.x == other.x and self.y == other.y
-
-    def __repr__(self) -> str:
-        return f'{self.__class__.__name__}(x={self.x}, y={self.y})'
-
-    def __add__(self, other_position: 'IntegerPosition2D') -> 'IntegerPosition2D':
-        x: int = self.x + other_position.x  # pylint: disable=invalid-name
-        y: int = self.y + other_position.y  # pylint: disable=invalid-name
-        sum_ = IntegerPosition2D(x, y)
-        return sum_
--- tests/illud/math/test_integer_position_2d.py
+++ tests/illud/math/test_integer_position_2d.py
@@ -1,84 +1,9 @@
 """Test illud.math.integer_position_2d."""
-from typing import Any
-
-import pytest
-
 from illud.math.integer_position_2d import IntegerPosition2D
+from illud.math.vector_2 import Vector2
+from testing.helpers.is_subclass_of_generic import is_subclass_of_generic
 
 
-# yapf: disable
-@pytest.mark.parametrize('x, y', [
-    (0, 0),
-    (1, 0),
-    (0, 1),
-    (1, 1),
-])  # pylint: disable=invalid-name
-# yapf: enable
-def test_init(x: int, y: int) -> None:
-    """Test illud.math.integer_position_2d.IntegerPosition2D.__init__."""
-    integer_position_2d = IntegerPosition2D(x, y)
-
-    assert integer_position_2d.x == x
-    assert integer_position_2d.y == y
-
-
-# yapf: disable
-@pytest.mark.parametrize('integer_position_2d, other, expected_equality', [
-    (IntegerPosition2D(0, 0), 'foo', False),
-    (IntegerPosition2D(0, 0), IntegerPosition2D(0, 1), False),
-    (IntegerPosition2D(0, 0), IntegerPosition2D(1, 0), False),
-    (IntegerPosition2D(0, 0), IntegerPosition2D(1, 1), False),
-    (IntegerPosition2D(0, 0), IntegerPosition2D(0, 0), True),
-    (IntegerPosition2D(0, 1), IntegerPosition2D(0, 1), True),
-    (IntegerPosition2D(1, 0), IntegerPosition2D(1, 0), True),
-    (IntegerPosition2D(1, 1), IntegerPosition2D(1, 1), True),
-])
-# yapf: enable
-def test_eq(integer_position_2d: IntegerPosition2D, other: Any, expected_equality: bool) -> None:
-    """Test illud.math.integer_position_2d.IntegerPosition2D.__eq__."""
-    equality: bool = integer_position_2d == other
-
-    assert equality == expected_equality
-
-
-# yapf: disable
-@pytest.mark.parametrize('integer_position_2d, expected_string', [
-    (IntegerPosition2D(0, 0), 'IntegerPosition2D(x=0, y=0)'),
-    (IntegerPosition2D(0, 1), 'IntegerPosition2D(x=0, y=1)'),
-    (IntegerPosition2D(1, 0), 'IntegerPosition2D(x=1, y=0)'),
-    (IntegerPosition2D(1, 1), 'IntegerPosition2D(x=1, y=1)'),
-])
-# yapf: enable
-def test_repr(integer_position_2d: IntegerPosition2D, expected_string: str) -> None:
-    """Test illud.math.integer_position_2d.IntegerPosition2D.__repr__."""
-    string: str = repr(integer_position_2d)
-
-    assert string == expected_string
-
-
-# yapf: disable
-@pytest.mark.parametrize('position, other_position, expected_sum', [
-    (IntegerPosition2D(0, 0), IntegerPosition2D(0, 0), IntegerPosition2D(0, 0)),
-    (IntegerPosition2D(0, 0), IntegerPosition2D(0, 1), IntegerPosition2D(0, 1)),
-    (IntegerPosition2D(0, 0), IntegerPosition2D(1, 0), IntegerPosition2D(1, 0)),
-    (IntegerPosition2D(0, 0), IntegerPosition2D(1, 1), IntegerPosition2D(1, 1)),
-    (IntegerPosition2D(0, 1), IntegerPosition2D(0, 0), IntegerPosition2D(0, 1)),
-    (IntegerPosition2D(0, 1), IntegerPosition2D(0, 1), IntegerPosition2D(0, 2)),
-    (IntegerPosition2D(0, 1), IntegerPosition2D(1, 0), IntegerPosition2D(1, 1)),
-    (IntegerPosition2D(0, 1), IntegerPosition2D(1, 1), IntegerPosition2D(1, 2)),
-    (IntegerPosition2D(1, 0), IntegerPosition2D(0, 0), IntegerPosition2D(1, 0)),
-    (IntegerPosition2D(1, 0), IntegerPosition2D(0, 1), IntegerPosition2D(1, 1)),
-    (IntegerPosition2D(1, 0), IntegerPosition2D(1, 0), IntegerPosition2D(2, 0)),
-    (IntegerPosition2D(1, 0), IntegerPosition2D(1, 1), IntegerPosition2D(2, 1)),
-    (IntegerPosition2D(1, 1), IntegerPosition2D(0, 0), IntegerPosition2D(1, 1)),
-    (IntegerPosition2D(1, 1), IntegerPosition2D(0, 1), IntegerPosition2D(1, 2)),
-    (IntegerPosition2D(1, 1), IntegerPosition2D(0, 0), IntegerPosition2D(1, 1)),
-    (IntegerPosition2D(1, 1), IntegerPosition2D(0, 1), IntegerPosition2D(1, 2)),
-])
-# yapf: enable
-def test_add(position: IntegerPosition2D, other_position: IntegerPosition2D,
-             expected_sum: IntegerPosition2D) -> None:
-    """Test illud.math.integer_position_2d.IntegerPosition2D.__add__."""
-    sum_ = position + other_position
-
-    assert sum_ == expected_sum
+def test_inheritance() -> None:
+    """Test illud.math.integer_position_2d.IntegerPosition2D inheritance."""
+    assert is_subclass_of_generic(IntegerPosition2D, Vector2[int])
