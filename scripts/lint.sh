252d684c767ee1b86808ae65a30b3fcc7ec97f49
Austin Scola <austinscola@gmail.com>
2021-02-15 18:40:10 -0500

    Change change directory method
    Change the way that scripts change directories.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,8 +5,7 @@ set -eu
 HERE="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 ILLUD="$(realpath "${HERE}/..")"
 
-pushd "${ILLUD}" > /dev/null
-trap "popd > /dev/null" EXIT
+cd "${ILLUD}"
 
 source "${ILLUD}/scripts/library/venv.sh"
 use_venv "test" frozen_test_requirements.txt
