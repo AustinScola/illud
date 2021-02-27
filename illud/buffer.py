98b4ab1be9892c3bfd24526bad0ddaf82e7827bc
Austin Scola <austinscola@gmail.com>
2021-02-26 19:04:09 -0500

    Add buffer length operator
    Add the ability to get the length of a buffer.

--- illud/buffer.py
+++ illud/buffer.py
@@ -26,6 +26,9 @@ class Buffer():
     def __getitem__(self, index: Union[int, slice]) -> str:
         return self.string.__getitem__(index)
 
+    def __len__(self) -> int:
+        return len(self.string)
+
     def index(self, substring: str, start: Optional[int] = None, end: Optional[int] = None) -> int:
         """Return the lowest index where the substring is found within the range. Raise ValueError
            if the substring is not found."""
