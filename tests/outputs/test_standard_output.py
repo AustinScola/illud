a67a6539576e4df7266180cabecba50b53f60d8c
Austin Scola <austinscola@gmail.com>
2021-03-19 06:29:04 -0400

    Add equality operator to the standard output
    Add an equality operator to the standard output so that we can more
    easily check that other objects which contain it (such as the canvas)
    can be compared for equality with the Seligimus decorators. The
    decorator for comparing instance attributes doesn't yet support
    excluding specific attributes, otherwise we could use that to ignore the
    standard output attribute.

--- tests/outputs/test_standard_output.py
+++ tests/outputs/test_standard_output.py
@@ -1,5 +1,6 @@
 """Test illud.outputs.standard_output."""
 import sys
+from typing import Any
 from unittest.mock import MagicMock, patch
 
 import pytest
@@ -18,6 +19,19 @@ def test_init() -> None:
     standard_output: StandardOutput = StandardOutput()
 
     assert standard_output._stdout == sys.stdout  # pylint: disable=protected-access
+
+
+# yapf: disable
+@pytest.mark.parametrize('standard_output, other, expected_equality', [
+    (StandardOutput(), 'foo', False),
+    (StandardOutput(), StandardOutput(), True),
+])
+# yapf: enable
+def test_eq(standard_output: StandardOutput, other: Any, expected_equality: bool) -> None:
+    """Test illud.outputs.standard_output.StandardOutput.__eq__."""
+    equality: bool = standard_output == other
+
+    assert equality == expected_equality
 
 
 # yapf: disable
