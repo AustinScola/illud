0831e121eb81225163e34c466e56764216c1bc5b
Austin Scola <austinscola@gmail.com>
2021-03-24 20:07:35 -0400

    Add terminal show cursor

--- illud/terminal.py
+++ illud/terminal.py
@@ -3,7 +3,7 @@ import os
 
 from seligimus.maths.integer_size_2d import IntegerSize2D
 
-from illud.ansi.escape_codes.cursor import MOVE_CURSOR_HOME
+from illud.ansi.escape_codes.cursor import MOVE_CURSOR_HOME, SHOW_CURSOR
 from illud.ansi.escape_codes.erase import CLEAR_SCREEN
 from illud.ansi.escape_codes.screen import DISABLE_ALTERNATIVE_SCREEN, ENABLE_ALTERNATIVE_SCREEN
 from illud.character import Character
@@ -52,6 +52,11 @@ class Terminal():
     def move_cursor_home(self) -> None:
         """Move the terminal cursor to the top left position of the terminal."""
         self._standard_output.write(MOVE_CURSOR_HOME)
+        self._standard_output.flush()
+
+    def show_cursor(self) -> None:
+        """Show the terminal cursor."""
+        self._standard_output.write(SHOW_CURSOR)
         self._standard_output.flush()
 
     def update(self) -> None:
