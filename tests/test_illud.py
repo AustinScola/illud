b083ad30cc8583e4b680e2c809aa844d0fb9e03f
Austin Scola <austinscola@gmail.com>
2021-04-02 06:59:47 -0400

    Add printing of the status bar

--- tests/test_illud.py
+++ tests/test_illud.py
@@ -26,6 +26,7 @@ from illud.selection import Selection
 from illud.signal_ import Signals
 from illud.signal_handler import SignalHandler
 from illud.signals.terminal_size_change import TerminalSizeChange
+from illud.status_bar import StatusBar
 from illud.terminal import Terminal
 from illud.window import Window
 
@@ -126,10 +127,11 @@ def test_evaluate(initial_state: IlludState, input_: IlludInput,
 
 # yapf: disable # pylint: disable=line-too-long
 @pytest.mark.parametrize('illud_state, result, expected_output', [
-    (IlludState(window=Window(size=IntegerSize2D(1, 1)), canvas=Canvas(IntegerSize2D(1, 1)).fill(' ')), None, '\x1b[;H \x1b[;H\x1b[7m \x1b[;2H\x1b[m'),
-    (IlludState(cursor=Cursor(Buffer('foo')), window=Window(size=IntegerSize2D(3, 1), buffer_=Buffer('foo')), canvas=Canvas(IntegerSize2D(3, 1)).fill(' ')), None, '\x1b[;Hfoo\x1b[;H\x1b[7mf\x1b[;2H\x1b[m'),
-    (IlludState(cursor=Cursor(Buffer('foo'), 1), window=Window(size=IntegerSize2D(3, 1), buffer_=Buffer('foo')), canvas=Canvas(IntegerSize2D(3, 1), inversions=[IntegerPosition2D()]).fill(' ')), None, '\x1b[;Hfoo\x1b[;2H\x1b[7mo\x1b[;3H\x1b[m'),
-    (IlludState(mode=Select(), selection=Selection(Buffer('foo'), 1), window=Window(size=IntegerSize2D(3, 1), buffer_=Buffer('foo')), canvas=Canvas(IntegerSize2D(3, 1), inversions=[IntegerPosition2D()]).fill(' ')), None, '\x1b[;Hfoo\x1b[;2H\x1b[7mo\x1b[;3H\x1b[m'),
+    (IlludState(window=Window(size=IntegerSize2D(1, 1)), canvas=Canvas(IntegerSize2D(1, 1), [[' ']])), None, '\x1b[;H \x1b[;H\x1b[7m \x1b[;2H\x1b[m'),
+    (IlludState(cursor=Cursor(Buffer('foo')), window=Window(size=IntegerSize2D(3, 1), buffer_=Buffer('foo')), canvas=Canvas(IntegerSize2D(3, 1), [[' ', ' ', ' ']])), None, '\x1b[;Hfoo\x1b[;H\x1b[7mf\x1b[;2H\x1b[m'),
+    (IlludState(cursor=Cursor(Buffer('foo'), 1), window=Window(size=IntegerSize2D(3, 1), buffer_=Buffer('foo')), canvas=Canvas(IntegerSize2D(3, 1), [[' ', ' ', ' ']], inversions=[IntegerPosition2D()])), None, '\x1b[;Hfoo\x1b[;2H\x1b[7mo\x1b[;3H\x1b[m'),
+    (IlludState(mode=Select(), selection=Selection(Buffer('foo'), 1), window=Window(size=IntegerSize2D(3, 1), buffer_=Buffer('foo')), canvas=Canvas(IntegerSize2D(3, 1), [[' ', ' ', ' ']], inversions=[IntegerPosition2D()])), None, '\x1b[;Hfoo\x1b[;2H\x1b[7mo\x1b[;3H\x1b[m'),
+    (IlludState(window=Window(size=IntegerSize2D(3, 2)), status_bar=StatusBar(position=IntegerPosition2D(0, 2), size=IntegerSize2D(3, 1)), canvas=Canvas(IntegerSize2D(3, 3))), None, '\x1b[;H   \x1b[2;H   \x1b[3;H   \x1b[;H\x1b[7m \x1b[;2H\x1b[m\x1b[3;H\x1b[7m \x1b[3;2H\x1b[m\x1b[3;2H\x1b[7m \x1b[3;3H\x1b[m\x1b[3;3H\x1b[7m \x1b[3;4H\x1b[m'),
 ])
 # yapf: enable # pylint: enable=line-too-long
 def test_print(illud_state: IlludState, result: Any, expected_output: str) -> None:
