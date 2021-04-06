3a7d9d065bcbebc3f9edb2fbb63525d144d10ff3
Austin Scola <austinscola@gmail.com>
2021-04-06 07:08:09 -0400

    Add backspace in normal mode
    Add the ability to use backspace in normal mode to remove the previous
    character and adjust the cursor appropriately.

--- tests/modes/test_normal.py
+++ tests/modes/test_normal.py
@@ -7,7 +7,7 @@ from seligimus.maths.integer_size_2d import IntegerSize2D
 
 from illud.buffer import Buffer
 from illud.character import Character
-from illud.characters import CONTROL_C
+from illud.characters import BACKSPACE, CONTROL_C
 from illud.cursor import Cursor
 from illud.exceptions.quit_exception import QuitException
 from illud.illud_state import IlludState
@@ -59,6 +59,7 @@ def test_inheritance() -> None:
     (IlludState(cursor=Cursor(Buffer('foo bar')), window=Window(size=IntegerSize2D(2, 1), buffer_=Buffer('foo bar'))), Character('w'), IlludState(cursor=Cursor(Buffer('foo bar'), 4), window=Window(size=IntegerSize2D(2, 1), buffer_=Buffer('foo bar'), offset=IntegerPosition2D(-3, 0))), False),
     (IlludState(cursor=Cursor(Buffer('foo'))), Character('x'), IlludState(cursor=Cursor(Buffer('oo')), clipboard=Buffer('f')), False),
     (IlludState(cursor=Cursor(Buffer('foobaz'), 3), clipboard=Buffer('bar')), Character('p'), IlludState(cursor=Cursor(Buffer('foobarbaz'), 6), clipboard=Buffer('bar')), False),
+    (IlludState(cursor=Cursor(Buffer('bar'), 3)), Character(BACKSPACE), IlludState(cursor=Cursor(Buffer('ba'), 2)), False),
     (IlludState(), Character(CONTROL_C), None, True),
 ])
 # yapf: enable # pylint: enable=line-too-long
